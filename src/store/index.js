b035be1bf47b056a3ebc107a4bb2d73418b9f647
dkit150750 <dkit150750@gmail.com>
2021-07-21 23:48:04 +0300

    export

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, createLogger } from 'vuex';
 
-import * as auth from '@/store/modules/Auth';
+import auth from '@/store/modules/Auth';
 import * as user from '@/store/modules/User';
 import * as message from '@/store/modules/Message';
 
