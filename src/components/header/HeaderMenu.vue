9f12e5a45bdd061fac9af5ca77dedc841c6169c0
dkit150750 <dkit150750@gmail.com>
2021-07-28 09:25:34 +0300

    menu

---
+++ src/components/header/HeaderMenu.vue
@@ -0,0 +1,123 @@
+<template>
+  <transition name="header-menu">
+    <div class="header-menu" v-if="isOpen" @click.self="$emit('close')">
+      <div class="header-menu__content">
+        <ul class="header-menu__list">
+          <li class="header-menu__item">
+            <router-link class="header-menu__link" :to="{ name: 'profile' }">
+              Профиль
+            </router-link>
+          </li>
+          <li class="header-menu__item">
+            <a href="" class="header-menu__link">Админка</a>
+          </li>
+          <li class="header-menu__item">
+            <button class="header-menu__link" type="button" @click="logout">
+              Выйти
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'HeaderMenu',
+
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  emits: {
+    close: null,
+  },
+
+  methods: {
+    ...mapActions('auth', ['logout']),
+  },
+};
+</script>
+
+<style>
+.header-menu {
+  position: absolute;
+  top: calc(100% - 10px);
+  right: 16px;
+  z-index: 2000;
+}
+
+@media (min-width: 550px) {
+  .header-menu {
+    top: calc(100% - 16px);
+    right: 40px;
+  }
+}
+
+.header-menu::before {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
+  content: '';
+}
+
+.header-menu-enter-active {
+  transition: all 0.15s ease-out;
+}
+
+.header-menu-leave-active {
+  transition: all 0.15s ease-in;
+}
+
+.header-menu-enter-from,
+.header-menu-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+.header-menu__content {
+  width: 270px;
+  padding: 8px 0;
+  border-radius: 16px;
+  background-color: var(--color-gray-0);
+  box-shadow: 0px 4px 16px rgb(0, 0, 0, 0.1),
+    0px 4px 22px 10px rgb(0, 0, 0, 0.04);
+}
+
+.header-menu__list {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+.header-menu__link {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 52px;
+  padding: 0 15px;
+  line-height: 20px;
+  font-size: 16px;
+  font-family: inherit;
+  text-decoration: none;
+  color: var(--ui-sb-color-text-main);
+  border: 0;
+  background-color: transparent;
+  cursor: pointer;
+  outline: none;
+}
+
+.header-menu__link:focus,
+.header-menu__link:hover {
+  background-color: var(--profile-card-backgrouind);
+}
+</style>
--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -2,10 +2,18 @@
   <header class="header">
     <div class="header__container">
       <HeaderLogo />
-      <template v-if="loggedIn">
-        <router-link class="header__link" :to="{ name: 'profile' }">
-          Профиль
-        </router-link>
+      <template v-if="!loggedIn">
+        <button
+          :class="burgerClass"
+          class="burger header__burger"
+          aria-label="открыть меню"
+          @click="openMenu"
+        >
+          <!-- <img src="" alt="аватар" /> -->
+          <span class="burger__line"></span>
+          <span class="burger__line"></span>
+          <span class="burger__line"></span>
+        </button>
       </template>
       <template v-else>
         <router-link class="header__link" :to="{ name: 'login' }">
@@ -13,6 +21,7 @@
           Войти
         </router-link>
       </template>
+      <HeaderMenu :isOpen="isOpenMenu" @close="isOpenMenu = false" />
     </div>
   </header>
 </template>
@@ -21,6 +30,7 @@
 import { mapGetters } from 'vuex';
 import HeaderLinkIcon from '@/components/header/HeaderLinkIcon.vue';
 import HeaderLogo from '@/components/header/HeaderLogo.vue';
+import HeaderMenu from '@/components/header/HeaderMenu.vue';
 
 export default {
   name: 'TheHeader',
@@ -28,22 +38,48 @@ export default {
   components: {
     HeaderLogo,
     HeaderLinkIcon,
+    HeaderMenu,
+  },
+
+  data() {
+    return {
+      isOpenMenu: false,
+    };
   },
 
   computed: {
     ...mapGetters('auth', ['loggedIn']),
+    burgerClass() {
+      return {
+        'burger--close': this.isOpenMenu,
+      };
+    },
+  },
+
+  methods: {
+    openMenu() {
+      this.isOpenMenu = !this.isOpenMenu;
+    },
   },
 };
 </script>
 
 <style>
 .header__container {
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: space-between;
   max-width: var(--max-width);
-  padding: 30px 16px;
+  height: 84px;
+  padding: 10px 16px;
   margin: 0 auto;
+}
+
+@media (min-width: 768px) {
+  .header__container {
+    height: 108px;
+  }
 }
 
 @media (min-width: 550px) {
@@ -82,5 +118,41 @@ export default {
 
 .header__link-icon {
   margin-right: 5px;
+}
+
+.burger {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 40px;
+  height: 40px;
+  padding: 8px 8px;
+  margin: 0;
+  background-color: transparent;
+  border: 0;
+  cursor: pointer;
+}
+
+.burger__line {
+  display: block;
+  width: 100%;
+  height: 2px;
+  border-radius: 4px;
+  background-color: var(--color-gray-900);
+}
+
+.burger--close {
+  justify-content: center;
+}
+
+.burger--close .burger__line:nth-child(3) {
+  display: none;
+}
+.burger--close .burger__line:nth-child(1) {
+  transform: translateY(50%) rotate(45deg);
+}
+
+.burger--close .burger__line:nth-child(2) {
+  transform: translateY(-50%) rotate(-45deg);
 }
 </style>
