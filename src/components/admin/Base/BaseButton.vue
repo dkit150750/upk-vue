9b95ee4ac80b0fc5e7859cb29f8bc64cce9456c7
dkit150750 <dkit150750@gmail.com>
2021-07-29 09:52:03 +0300

    базовая кнопка

---
+++ src/components/admin/Base/BaseButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="admin-base-button" :type="type">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+
+  props: {
+    type: {
+      type: String,
+      default: 'button',
+    },
+  },
+};
+</script>
+
+<style>
+.admin-base-button {
+  display: block;
+  padding: 13px 20px;
+  font-family: inherit;
+  font-size: 16px;
+  color: var(--color-gray-50);
+  cursor: pointer;
+  background-color: var(--color-primary-800);
+  border: 0;
+  border-radius: 6px;
+}
+</style>
