bd85cabe2dbe63c2a2ea68536d2d82c57f13dca2
dkit150750 <dkit150750@gmail.com>
2021-07-29 09:51:53 +0300

    добавил метода

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -4,4 +4,10 @@ export default {
   getCourses() {
     return API.apiClient.get(`/courses`);
   },
+  addCourse() {
+    return API.apiClient.post(`/courses`);
+  },
+  getCourse(id) {
+    return API.apiClient.get(`/courses/${id}`);
+  },
 };
