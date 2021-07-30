b3e7866b4e8b3996d7bdceb44bcb89bdcae2b380
dkit150750 <dkit150750@gmail.com>
2021-07-31 00:36:01 +0300

    TheCourse

--- src/views/admin/TheCourse.vue
+++ src/views/admin/TheCourse.vue
@@ -1,26 +1,28 @@
 <template>
   <h1 class="main__title">Инофрмация о курсе</h1>
-  <CourseInfo class="main__course-info">
-    <CourseInfoField
-      class="input-group__input--textarea-name"
+  <CourseInfo class="main__course-info" @updateCourse="updateCourse">
+    <BaseField
       label="Название"
       id="name"
       name="name"
+      type="text"
       v-model.trim="course.title"
     />
-    <CourseInfoField
+    <BaseField
       label="Описание"
       id="description"
       name="description"
       v-model.trim="course.description"
     />
     <CourseInfoPicture
+      class="admin-course-info__picture"
       :id="course.id"
       :title="course.title"
       :picture="course.picture"
       @updatePicture="setPicture"
     />
-    <CourseInfoField
+    <BaseField
+      class="admin-course-info__background"
       label="Цвет"
       id="background"
       name="background"
@@ -28,37 +30,45 @@
       v-model.trim="course.background"
     />
   </CourseInfo>
-  <CourseInfoDates v-if="course.dates" :dates="course.dates" />
+  <CourseInfoLectureAdd />
+  <CourseInfoLectures v-if="lectures.length" :lectures="lectures" />
+  <FlashMessage :message="message" @close="message = null" />
 </template>
 
 <script>
 import CourseService from '@/services/CourseService';
 import { getErrorData } from '@/utils/helpers';
 
+import BaseField from '@/components/admin/base/BaseField.vue';
+import FlashMessage from '@/components/FlashMessage.vue';
 import CourseInfo from '@/components/admin/course/CourseInfo.vue';
-import CourseInfoField from '@/components/admin/course/CourseInfoField.vue';
 import CourseInfoPicture from '@/components/admin/course/CourseInfoPicture.vue';
-import CourseInfoDates from '@/components/admin/course/CourseInfoDates.vue';
+import CourseInfoLectureAdd from '@/components/admin/course/CourseInfoLectureAdd.vue';
+import CourseInfoLectures from '@/components/admin/course/CourseInfoLectures.vue';
 
 export default {
   name: 'TheCourse',
 
   components: {
+    BaseField,
     CourseInfo,
-    CourseInfoField,
     CourseInfoPicture,
-    CourseInfoDates,
+    CourseInfoLectures,
+    CourseInfoLectureAdd,
+    FlashMessage,
   },
 
   data() {
     return {
       course: {
-        id: '',
+        id: 0,
         title: '',
         description: '',
         picture: '',
-        dates: [],
+        background: '',
       },
+      lectures: [],
+      message: null,
     };
   },
 
@@ -72,8 +82,27 @@ export default {
       }
     },
 
+    async updateCourse() {
+      console.log(1);
+      try {
+        const course = this.course;
+        const payload = {
+          id: course.id,
+          title: course.title,
+          description: course.description,
+          background: course.background,
+        };
+        const response = await CourseService.updateCourse(payload);
+        this.course = response.data.data;
+        this.lectures = this.course.lectures ?? [];
+      } catch (error) {
+        console.log(getErrorData(error));
+      }
+    },
+
     setPicture(picture) {
       this.course.picture = picture;
+      this.message = 'Данные обновлены';
     },
   },
 
