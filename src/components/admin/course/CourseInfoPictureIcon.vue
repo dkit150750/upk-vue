5a19e4485d82bf4a51f3049b1b7c503943bdf6ce
dkit150750 <dkit150750@gmail.com>
2021-07-30 08:47:49 +0300

    CourseInfoField

---
+++ src/components/admin/course/CourseInfoPictureIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    class="course-info-picture-icon"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M14.12 4l1.83 2H20v12H4V6h4.05l1.83-2h4.24zM15 2H9L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-3.17L15 2zm-3 7c1.65 0 3 1.35 3 3s-1.35 3-3 3-3-1.35-3-3 1.35-3 3-3zm0-2c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5z"
+    ></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'CourseInfoPictureIcon',
+};
+</script>
+
+<style>
+.course-info-picture-icon {
+  display: block;
+  height: 24px;
+  color: var(--color-gray-0);
+  pointer-events: none;
+}
+</style>
