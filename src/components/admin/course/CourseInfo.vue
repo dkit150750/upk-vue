dffb32be77ed8c7c0a64ddf4553089052f677cab
dkit150750 <dkit150750@gmail.com>
2021-07-31 00:36:27 +0300

    CourseInfo

--- src/components/admin/course/CourseInfo.vue
+++ src/components/admin/course/CourseInfo.vue
@@ -1,33 +1,95 @@
 <template>
-  <form class="admin-course-info">
-    <slot />
-    <button class="admin-course-info__save" type="submit">Сохранить</button>
-  </form>
+  <div class="admin-course-info">
+    <form class="admin-course-info__form" @submit.prevent="updateCourse">
+      <slot />
+      <div class="admin-course-info__buttons">
+        <BaseButton class="admin-course-info__save" type="submit">
+          Сохранить
+        </BaseButton>
+        <BaseButton class="admin-course-info__del">Удалить</BaseButton>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
+import BaseButton from '@/components/admin/base/BaseButton.vue';
+
 export default {
   name: 'CourseInfo',
+
+  components: {
+    BaseButton,
+  },
+
+  emits: {
+    updateCourse: null,
+  },
+
+  methods: {
+    updateCourse() {
+      console.log(2);
+      this.$emit('updateCourse');
+    },
+  },
 };
 </script>
 
 <style>
 .admin-course-info {
-  display: grid;
-  gap: 45px;
-  max-width: 500px;
+  max-width: 700px;
+  padding: 20px;
+  background-color: var(--color-gray-70);
+  border-radius: 10px;
 }
 
-.admin-course-info__save {
-  display: block;
-  justify-self: start;
-  padding: 13px 20px;
-  font-family: inherit;
-  font-size: 16px;
-  color: var(--color-gray-50);
-  cursor: pointer;
-  background-color: var(--color-primary-800);
-  border: 0;
-  border-radius: 6px;
+.admin-course-info__form {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 20px 25px;
+}
+
+@media (min-width: 640px) {
+  .admin-course-info__form {
+    grid-template-columns: 1fr 200px;
+  }
+}
+
+.admin-course-info__picture {
+  justify-self: center;
+}
+
+@media (min-width: 640px) {
+  .admin-course-info__picture {
+    align-self: flex-start;
+    grid-column: 2 / 3;
+    grid-row: 1 / 3;
+  }
+  .admin-course-info__background {
+    grid-column: 2 / 3;
+    grid-row: 3 / 4;
+  }
+}
+
+.admin-course-info__buttons {
+  display: grid;
+  gap: 20px;
+  align-items: flex-end;
+}
+
+.admin-course-info__del {
+  background-color: var(--color-red-800);
+}
+
+@media (min-width: 640px) {
+  .admin-course-info__buttons {
+    grid-column: 1 / 2;
+    grid-template-columns: min-content min-content;
+  }
+
+  .admin-course-info__save,
+  .admin-course-info__del {
+    justify-self: stretch;
+  }
 }
 </style>
